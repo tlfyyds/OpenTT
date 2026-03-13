781fac963e4ec70a9c03c017dda5abcc235c5f86
bob <101806772+tlfyyds@users.noreply.github.com>
2026-03-13 16:27:11 +0800

    Delete mcps directory

--- mcps/base.py
+++
@@ -1,8 +0,0 @@
-class MCPTool:
-    def get_definition(self):
-        """返回符合 OpenAI/Ollama 格式的工具定义"""
-        raise NotImplementedError
-        
-    def execute(self, **kwargs):
-        """执行工具逻辑"""
-        raise NotImplementedError
